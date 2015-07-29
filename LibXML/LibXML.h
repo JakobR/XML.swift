ce4c7a9f699b7f8b5df47c66275f95ef7079f31d
Jakob Rath <mail@jakobrath.eu>
2015-07-29 22:05:32 +0200

    Initial Commit

---
+++ LibXML/LibXML.h
@@ -0,0 +1,19 @@
+//
+//  LibXML.h
+//  LibXML
+//
+//  Created by Jakob Rath on 29/07/15.
+//  Copyright © 2015 Jakob Rath. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LibXML.
+FOUNDATION_EXPORT double LibXMLVersionNumber;
+
+//! Project version string for LibXML.
+FOUNDATION_EXPORT const unsigned char LibXMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibXML/PublicHeader.h>
+
+
