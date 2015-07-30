026e299cffdb96d09fa44c919141347c146aee39
Jakob Rath <mail@jakobrath.eu>
2015-07-30 12:43:30 +0200

    Add ParserContext wrapper.

---
+++ LibXML/ParserContext.swift
@@ -0,0 +1,30 @@
+//
+//  ParserContext.swift
+//  LibXML
+//
+//  Created by Jakob Rath on 30/07/15.
+//  Copyright © 2015 Jakob Rath. All rights reserved.
+//
+
+import libxml2
+
+/// Owns an xmlParserCtxtPtr and ensures it is cleaned up correctly.
+class ParserContext {
+    let ptr: xmlParserCtxtPtr
+
+    init() throws {
+        ptr = xmlNewParserCtxt()
+        guard ptr != nil else {
+            throw Error.MemoryError
+        }
+    }
+
+    deinit {
+        print("Calling xmlFreeParserCtxt...")
+        xmlFreeParserCtxt(ptr)
+    }
+
+    var isValid: Bool {
+        return ptr.memory.valid != 0;
+    }
+}
