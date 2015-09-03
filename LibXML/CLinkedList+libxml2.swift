68b9212c1e6575b749e1cdb47651b703e41dd0fe
Jakob Rath <mail@jakobrath.eu>
2015-09-03 11:13:43 +0200

    Add attribute-related methods to Node

--- LibXML/CLinkedList+libxml2.swift
+++ LibXML/CLinkedList+libxml2.swift
@@ -11,3 +11,7 @@ import libxml2
 extension _xmlNode: CLinkedListNext {
     var cLinkedListNext: UnsafeMutablePointer<_xmlNode> { return self.next }
 }
+
+extension _xmlAttr: CLinkedListNext {
+    var cLinkedListNext: UnsafeMutablePointer<_xmlAttr> { return self.next }
+}
--- LibXML/Node.swift
+++ LibXML/Node.swift
@@ -11,24 +11,30 @@ import libxml2
 
 public class Node {
     private let ptr: xmlNodePtr
-    private let keepAlive: Any
+    private let keepAlive: libxmlDoc
 
-    init(_ xmlNode: xmlNodePtr, keepAlive: Any) {
+    init(_ xmlNode: xmlNodePtr, keepAlive: libxmlDoc) {
         self.ptr = xmlNode
         self.keepAlive = keepAlive
     }
 
-    public lazy var children: [Node] = CLinkedList(self.ptr.memory.children).map { Node($0, keepAlive: self.keepAlive) }
-    /*
-    {
-        var children: [Node] = []
-        for var nodePtr = self.ptr.memory.children; nodePtr != nil; nodePtr = nodePtr.memory.next {
-            if xmlNodeIsText(nodePtr) == 0 {
-                let child = Node(nodePtr, keepAlive: self.keepAlive)
-                children.append(child)
-            }
-        }
-        return children
-    }()
-    */
+    public private(set) lazy var name: String? = String.fromCString(self.ptr.memory.name)
+
+    public private(set) lazy var children: [Node] = CLinkedList(self.ptr.memory.children).map { Node($0, keepAlive: self.keepAlive) }
+
+    public private(set) lazy var attributes: [Attribute] = CLinkedList(self.ptr.memory.properties).map { Attribute($0, keepAlive: self.keepAlive) }
+
+    /// Attribute lookup (not namespace aware)
+    public func valueForAttribute(name: String) -> String? {
+        let cs = xmlGetProp(ptr, name)
+        defer { xmlFree(cs) }
+        return String.fromCString(cs)
+    }
+
+    /// Namespace-aware attribute lookup
+    public func valueForAttribute(name: String, namespace: String) -> String? {
+        let cs = xmlGetNsProp(ptr, name, namespace)
+        defer { xmlFree(cs) }
+        return String.fromCString(cs)
+    }
 }
